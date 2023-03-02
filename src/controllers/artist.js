518c80c12bbab4216aa8620a093418fba972cccf
CodeLHC <lhcrawford97@gmail.com>
2023-03-02 08:35:06 +0000

    db query to insert new artists into artist table

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,5 +1,13 @@
-const createArtist = (req, res) => {
-    res.sendStatus(201)
+const db = require('../src/db/index.js');
+
+async function createArtist(req, res) {
+    const {name, genre} = req.body
+    try{
+        const { rows: [ artist ] }= await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+    res.sendStatus(201).json(artist)
+    } catch(error){
+        res.sendStatus(500).json(error.message)
+    }
 }
 
 module.exports = {
