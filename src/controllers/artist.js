fc65979a5da7f4b3931151ed89b382974ad431af
CodeLHC <lhcrawford97@gmail.com>
2023-03-02 18:33:21 +0000

    update to createArtist function to prevent malicious SQL

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -3,7 +3,7 @@ const db = require('../db/index.js');
 async function createArtist(req, res) {
     const {name, genre} = req.body
     try{
-        const { rows: [ artist ] }= await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+        const { rows: [ artist ] }= await db.query(`INSERT INTO Artists (name, genre) VALUES ('$1', '$2') RETURNING *,` [name, genre])
     res.sendStatus(201).json(artist)
     } catch(err){
         res.sendStatus(500).json(err.message)
