dd0d25c2b7d61afd3ef78b5e4b7af8ddc928e31e
CodeLHC <lhcrawford97@gmail.com>
2023-03-03 20:20:40 +0000

    retrieveArtists function

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -2,17 +2,27 @@ const db = require('../db/index.js');
 
 async function createArtist(req, res) {
     const {name, genre} = req.body
-    console.log({name});
-    console.log({genre});
+
     try{
-        const { rows: [ artist ] }= await db.query(`INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`, [name, genre])
-        console.log({artist})
+        const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *`, [name, genre])
+       
     res.status(201).json(artist)
     } catch (err) {
         res.status(500).json(err.message)
     }
 }
 
+async function retrieveArtists(req,res){
+    try{
+        const { rows } = await db.query(`SELECT * FROM Artists`)
+        
+    res.status(200).json(rows)
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+}
+
 module.exports = {
-    createArtist
+    createArtist,
+    retrieveArtists
 }
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,6 +3,9 @@ const express = require('express');
 const artistController = require('../controllers/artist.js');
 const router = express.Router();
 
+router.get('/', artistController.readArtists)
+
 router.post('/', artistController.createArtist)
+
 
 module.exports = router;
