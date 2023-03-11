efd76bd1eacd95d21a9e86fa6d2a26fb4541ef20
CodeLHC <lhcrawford97@gmail.com>
2023-03-11 21:15:26 +0000

    update and delete album tests and functions

--- src/controllers/albums.js
+++ src/controllers/albums.js
@@ -13,7 +13,7 @@ async function createAlbum(req, res) {
     }
 }
 
-const retrieveAlbum = async (req, res) => {
+const retrieveAlbums = async (req, res) => {
     try {
       const { rows } = await db.query('SELECT * FROM Albums');
       res.status(200).json(rows);
@@ -38,8 +38,62 @@ const retrieveAlbum = async (req, res) => {
     }
   };
 
+  const updateAlbum = async (req, res) => {
+    const { id } = req.params;
+    const { name, year } = req.body;
+  
+    let query, params;
+  
+    if (name && year) {
+      query = `UPDATE Albums SET name = $1, year = $2 WHERE id = $3 RETURNING name , year, id`;
+      params = [name, year, id];
+    } else if (name) {
+      query = `UPDATE Albums SET name = $1 WHERE id = $2 RETURNING name , year, id`;
+      params = [name, id];
+    } else if (year) {
+      query = `UPDATE Albums SET year = $1 WHERE id = $2 RETURNING name , year, id`;
+      params = [year, id];
+    }
+    try {
+      const {
+        rows: [albums],
+      } = await db.query(query, params);
+  
+      if (!albums) {
+        return res.status(404).json({ message: `Album ${id} does not exist` });
+      }
+      res.status(200).json(albums);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err.message);
+    }
+  };
+  
+  const deleteAlbum = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { rows } = await db.query(
+        'DELETE FROM Albums WHERE id = $1 RETURNING id , name, year',
+        [id]
+      );
+  
+      if (rows.length === 0) {
+        return res.status(404).json({
+          message: `The album ID: ${id} does not exist`,
+        });
+      }
+  
+      const deletedAlbum = rows[0];
+      res.status(200).json(deletedAlbum);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
+  };
+
 module.exports = {
     createAlbum,
-    retrieveAlbum,
-    getAlbumById
+    retrieveAlbums,
+    getAlbumById,
+    updateAlbum,
+    deleteAlbum
 }
--- src/routes/albums.js
+++ src/routes/albums.js
@@ -1,17 +1,11 @@
 const express = require('express');
-
-const artistController = require('../controllers/artist.js');
 const albumController = require('../controllers/albums.js');
 const router = express.Router();
 
-router.get('/', albumController.retrieveAlbum);
-
+router.get('/', albumController.retrieveAlbums);
 router.get('/:id', albumController.getAlbumById);
-
 router.post('/:id/albums', albumController.createAlbum);
-
-//router.patch('/:id', albumController.updateAlbum);
-
-//router.delete('/:id', albumController.deleteAlbum);
+router.patch('/:id', albumController.updateAlbum);
+router.delete('/:id', albumController.deleteAlbum);
 
 module.exports = router;
